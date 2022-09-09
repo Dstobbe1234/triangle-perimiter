58c7ccb5215a9f9256a99bec1500018bcd93fe44
David Stobbe <d.stobbe1@share.epsb.ca>
2022-09-09 17:17:05 -0600

    finished html, started js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // Triangle Perimeter Assignment Start Code
+
+let calcBtn = document.getElementById('calc');
+calcBtn.addEventListener("click", btnClicked)
+
+function btnClicked() {
+    let xA = document.getElementById("")
+}
